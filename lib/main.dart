827605839c102def0f94f848329f31a0e49b5ae5
The Shark <danmpinda82@gmail.com>
2024-08-15 15:09:23 +0300

    displaying the time

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:world_time/loading.dart';
+import 'package:world_time/pages/loading.dart';
 import 'package:world_time/pages/chose_location.dart';
 import 'package:world_time/pages/home.dart';
 
---
+++ lib/services/world_time.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WorldTime {
+  String location; //location name for the UI
+  String time = ''; //time for the location chosen
+  String flag; //flag of the location
+  String url; //url for the location
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    final response =
+        await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    print('$offset');
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+//set time property
+    time = now.toString();
+  }
+}
